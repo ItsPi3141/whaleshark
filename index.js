cab1ee6d0cc33de40dac43da8ebf31deb2e23e47
Pi <piisthebestthingintheuniverse@gmail.com>
2024-06-01 18:30:27 -0700

    bot: error handling

--- index.js
+++ index.js
@@ -64,3 +64,21 @@ client.on("messageCreate", (message) => {
 });
 
 client.login(process.env.TOKEN);
+
+// dont crash
+process.on("unhandledRejection", (reason, p) => {
+	console.log(" [Error_Handling] :: Unhandled Rejection/Catch");
+	console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+	console.log(" [Error_Handling] :: Uncaught Exception/Catch");
+	console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+	console.log(" [Error_Handling] :: Uncaught Exception/Catch (MONITOR)");
+	console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+	console.log(" [Error_Handling] :: Multiple Resolves");
+	console.log(type, promise, reason);
+});
