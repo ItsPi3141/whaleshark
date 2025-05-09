9b58da0f83a9b181129f63b5fce5764d16e71138
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-05-08 20:26:18 -0700

    feat: switch to gemma api for chat

--- commands/chat.js
+++ commands/chat.js
@@ -1,38 +1,90 @@
 const config = require("../config.json");
 const encode3y3 = require("../3y3.js");
+const { GoogleGenAI } = require("@google/genai");
+
+const SYSTEM_PROMPT = process.env.SYSTEM_PROMPT.replaceAll(
+	"\\n",
+	"\n",
+).replaceAll('\\"', '"');
 
 let uniqueId = Date.now();
+/** @type {Record<string, import("@google/genai").Content[]>} */
+let conversations = {};
+
+const ai = new GoogleGenAI({
+	apiKey: process.env.GEMINI_API_KEY,
+});
 
 module.exports = {
 	name: "chat",
 	cooldown: 1,
-	execute(message, args) {
+	async execute(message, args) {
 		if (args.length === 0) {
 			message.reply({
-				embeds: [new EmbedBuilder().setDescription("Talk to WhaleShark\n```\nws chat <text>\n```")],
+				embeds: [
+					new EmbedBuilder().setDescription(
+						"Talk to WhaleShark\n```\nws chat <text>\n```",
+					),
+				],
 			});
 		} else {
-			message.reply(`${config.emojis.loading}⠀`).then((reply) => {
-				fetch(process.env.SMYTHOS_GPT_ENDPOINT, {
-					headers: {
-						accept: "text/plain",
-						"accept-language": "en-US,en;q=0.9",
-						"content-type": "application/json",
-					},
-					body: JSON.stringify({
-						prompt: `${message.author.tag}: ${args.join(" ")}`,
-						channelId: `${message.channel.id}-${uniqueId}`,
-					}),
-					method: "POST",
-				})
-					.then((r) => r.json())
-					.then((json) => {
-						reply.edit(json.response + encode3y3.encode("ai"));
+			const reply = await message.reply(`${config.emojis.loading}⠀`);
+			try {
+				const cId = `${message.channel.id}-${uniqueId}`;
+				if (!conversations[cId]) {
+					conversations[cId] = [];
+					conversations[cId].push({
+						role: "user",
+						parts: [
+							{
+								text: SYSTEM_PROMPT,
+							},
+						],
 					});
-			});
+				}
+				conversations[cId].push({
+					role: "user",
+					parts: [
+						{
+							text: `${message.author.username}: ${args.join(" ")}`,
+						},
+					],
+				});
+				const { text } = await ai.models.generateContent({
+					model: "gemma-3-27b-it",
+					contents: conversations[cId],
+				});
+				let response = text;
+				for (const r of ["<start_of_turn>", "<end_of_turn>"]) {
+					response = response.replaceAll(r, "");
+				}
+				response = response.trim();
+				conversations[cId].push({
+					role: "model",
+					parts: [
+						{
+							text: response,
+						},
+					],
+				});
+
+				if (conversations[cId].length > 100) {
+					conversations[cId] = conversations[cId].slice(-100);
+				}
+
+				await reply.edit(response + encode3y3.encode("ai"));
+			} catch (e) {
+				console.error(e);
+				try {
+					await reply.edit("❌ Something went wrong.");
+				} catch {
+					await message.reply("❌ Something went wrong.");
+				}
+			}
 		}
 	},
 	reloadUniqueId: () => {
 		uniqueId = Date.now();
+		conversations = {};
 	},
 };
